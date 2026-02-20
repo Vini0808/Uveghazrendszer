e85fdd3040ed44d80e245eaf1a8531288d439dbd
Diak <vs@petrik.hu>
2026-02-20 09:27:49 +0100

    Kesz Novenyfaj

--- Uveghazrendszer/NovenyFaj.cs
+++ Uveghazrendszer/NovenyFaj.cs
@@ -8,5 +8,36 @@ namespace Uveghazrendszer
 {
 	internal class NovenyFaj
 	{
+		string nev;
+		int nedvessegtartalom; //100 ideális esetben
+		int egeszsegSzint; //100 a max,  60 alatt beteg lesz
+		int optimalisSuruseg; //
+
+		public NovenyFaj(string nev, int optimalisSuruseg)
+		{
+			this.nev = nev;
+			this.optimalisSuruseg = optimalisSuruseg;
+			this.nedvessegtartalom = 100;
+			this.egeszsegSzint = 100;
+		}
+
+		public string Nev { get => nev; set => nev = value; }
+		public int Nedvessegtartalom { get => nedvessegtartalom; set => nedvessegtartalom = value; }
+		public int EgeszsegSzint { get => egeszsegSzint; set => egeszsegSzint = value; }
+		public int OptimalisSuruseg { get => optimalisSuruseg; set => optimalisSuruseg = value; }
+
+		//Azonosító név
+		public string Azonosito
+		{
+			get
+			{
+				return this.nev.Substring(0,3);
+			}
+		}
+
+		public override string ToString()
+		{
+			return $"{this.nev} - Nedvességtartalom: {this.nedvessegtartalom}%, Egészségszint: {this.egeszsegSzint}%, Optimális sűrűség: {this.optimalisSuruseg}";
+		}
 	}
 }
