984075fdcd8aadef15621150e21bbb736c8ffd77
Diak <vs@petrik.hu>
2026-02-20 09:16:37 +0100

    KezeloKesz

---
+++ Uveghazrendszer/Enumok.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uveghazrendszer
+{
+
+	//Szerepkor: kertesz, Technikus, Admin
+	enum Szerepkor
+	{
+		KERTESZ,
+		TECHNIKUS,
+		ADMIN
+	}
+
+	internal class Enumok
+	{
+	}
+}
---
+++ Uveghazrendszer/Kezelo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uveghazrendszer
+{
+	internal class Kezelo
+	{
+		string nev;
+		string azonosito;
+		Szerepkor szerepkor;
+
+		public Kezelo(string nev, string azonosito, Szerepkor szerepkor)
+		{
+			this.nev = nev;
+			this.azonosito = azonosito;
+			this.szerepkor = szerepkor;
+		}
+
+		public string Nev { get => nev; set => nev = value; }
+		public string Azonosito { get => azonosito; set => azonosito = value; }
+		public Szerepkor Szerepkor { get => szerepkor; set => szerepkor = value; }
+
+		public override string ToString()
+		{
+			return $"{this.nev} ({this.azonosito}) - {this.szerepkor}";
+		}
+	}
+}
